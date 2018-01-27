e605833ce146553919ef3d459272d65bafa02669
saurabhiet <35842602+saurabhiet@users.noreply.github.com>
2018-01-27 23:34:22 +0530

    Add files via upload

---
+++ Session2_Assignment2.py
@@ -0,0 +1,14 @@
+﻿for x in range(1, 6):
+    t = 1
+    st="x"
+    while(t<x):
+        st = st + "x"
+        t += 1
+    print(st)
+for x in range(4, 0, -1):
+    t = 1
+    st="x"
+    while(t<x):
+        st = st + "x"
+        t += 1
+    print(st)
